9c01e5fdd63307c2362618adbc2b7b2d2cf74bad
Liam Hogan <hoganliam3@gmail.com>
2020-03-27 13:22:03 -0400

    Adding player class file.

---
+++ Boston Celtics/Player.swift
@@ -0,0 +1,23 @@
+//
+//  Player.swift
+//  Boston Celtics
+//
+//  Created by Liam Hogan on 3/27/20.
+//  Copyright © 2020 Liam Hogan. All rights reserved.
+//
+
+import Foundation
+
+class Player{
+    var playerName:String;
+    var playerNumber:String;
+    var playerPosition:String;
+    var playerCollege:String;
+    
+    init(playerName:String, playerNumber:Int, playerPosition:String, playerCollege:String) {
+        self.playerName = playerName;
+        self.playerNumber = "\(playerNumber)";
+        self.playerPosition = playerPosition;
+        self.playerCollege = playerCollege;
+    }
+}
